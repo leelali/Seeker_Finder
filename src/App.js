2e8a7d22b9417a7b0e21e1cf9b2af876660834a9
Xiaoping Li <narci2008@gmail.com>
2018-06-24 16:38:22 -0700

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Signout from './components/Signout';
+import Seeker from './components/Seeker';
+import Finder from './components/Finder';
+import Account from './components/Account';
+
+
 import './App.css';
 
 class App extends Component {
@@ -8,13 +18,13 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Navigation />
-        <Route path='/' exact comopnent={} />
-        <Route path='/signup' comopnent={} />
-        <Route path='/signin' comopnent={} />
-        <Route path='/signout' comopnent={} />
-        <Route path='/seeker' comopnent={} />
-        <Route path='/finder' comopnent={} />
-        <Route path='/account' comopnent={} />
+        <Route path='/' exact comopnent={Home} />
+        <Route path='/signup' comopnent={Signup} />                 
+        <Route path='/signin' comopnent={Signin} />
+        <Route path='/signout' comopnent={Signout} />
+        <Route path='/seeker' comopnent={Seeker} />
+        <Route path='/finder' comopnent={Finder} />
+        <Route path='/account' comopnent={Account} />
       </BrowserRouter>
     );
   }
---
+++ src/components/Seeker.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Seeker extends Component {
+  render() {
+    return (
+      <div>Welcome</div>
+    );
+  }
+}
+
+export default Seeker;
