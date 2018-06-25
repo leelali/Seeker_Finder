18e5ee0011f72808d978b10cd554dc9a09df01c7
Xiaoping Li <narci2008@gmail.com>
2018-06-25 12:55:48 -0700

    Add input fields for seeker component

--- src/components/Seeker.js
+++ src/components/Seeker.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
 
 class Seeker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      itemType: '',
+      image: null,
+      description: '',
+      address: '',
+      zipCode: '',
+    };
+  }
+
+  handleInputChange = (e)=> {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
   render() {
     return (
-      <div>I am Seeker</div>
+      <div>
+        Which type the missing item belongs to:
+        {/* creates a dropdown menu of missing items' types to choose from */}
+        <select className='dropdownList' name='itemType' onChange={this.handleInputChange}>
+              <option value='' selected disabled>Select A Item Type</option>
+              <option value='Human Being'>Human Being</option>
+              <option value='Pets'>Pets</option>
+              <option value='Cards'>Cards</option>
+              <option value='Clothes'>Clothes</option>
+              <option value='Keys Wallet'>Keys Wallet</option>
+              <option value='Toys'>Toys</option>
+              <option value='Others'>Others</option>
+          </select>
+          <div>
+            Please Add Description for the missing item:
+            <input type='text' name='description' value={this.state.description} onChange={this.handleInputChange} placeholder='Enter Description for Missing Item'></input>
+          </div>
+          <div>Where did you last time see the missing item?</div>
+          <div>
+            Address:
+            <input type='text' name='address' value={this.state.address} onChange={this.handleInputChange} placeholder='Enter the Address'></input>
+          </div>
+          <div>
+            Zipcode:
+            <input type='number' name='zipCode' value={this.state.zipCode} onChange={this.handleInputChange} placeholder='Enter Zip Code'></input>
+          </div>
+          <button>Submit</button>
+      </div>
     );
   }
 }
--- src/components/Signout.js
+++ src/components/Signout.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Signout extends Component {
   render() {
     return (
-      <div>Please sign out</div>
+      <div>Sign out successfully</div>
     );
   }
 }
