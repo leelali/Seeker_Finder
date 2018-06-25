772d9984972f9b1151a93995294c0012a845bb22
Xiaoping Li <narci2008@gmail.com>
2018-06-25 11:03:56 -0700

    Add dropdown list and zipcode input field to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userRole: '',
+      zipCode: null,
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
+    const userRoles = ['Seeker', 'Finder'];
     return (
-      <h1>Welcome to home</h1>
+      <div>
+        <div>
+          {/* creates a dropdown menu of Seeker/Finder to choose from */}
+          <select className='dropdownList' name='userRole' onChange={this.handleInputChange}>
+              <option value='' selected disabled>Select A User Role</option>
+              <option value='Seeker'>Seeker</option>
+              <option value='Finder'>Finder</option>
+          </select>
+        </div>
+        <div>
+          <input type='number' name='zipCode' value={this.state.zipCode} onChange={this.handleInputChange} placeholder='Enter Zip Code'></input>
+        </div>
+        <div>
+          <button>Start</button>
+        </div>
+        
+        
+        
+      </div>
     );
   }
 }
