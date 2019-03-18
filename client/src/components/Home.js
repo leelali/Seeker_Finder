5382f280ea0513a3ceeb48ee9145aa589096135e
Xiaoping-Li <narci2008@gmail.com>
2019-03-18 12:26:42 -0700

    Add Form for item information into Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,22 +5,52 @@ class Home extends Component {
     super();
     this.state = {
       postType: '',
+      images: [],
     };
   }
 
-  handleBtnClick = (e) => {
+  handleBtnClick = event => {
     this.setState({
-      postType: e.target.value
-    });
+      postType: event.target.value
+    }); 
+  }
+
+  handleImagesUpload = event => {
+    const image = event.target.files[0];
+    const updatedImages = this.state.images.concat(image);
+    this.setState({ images: updatedImages});
   }
 
   render() {
     return (
       <div>
-        <button value="Seeker" onClick={this.handleBtnClick}>Seeker</button>
-        <button value="Finder" onClick={this.handleBtnClick}>Finder</button>
-        {this.state.postType}
+        <div className="postType">
+          <button value="Seeker" onClick={this.handleBtnClick}>Seeker</button>
+          <button value="Finder" onClick={this.handleBtnClick}>Finder</button>
+        </div>
+
+        <form className="postInfo">
+          {/* upload images up to 3x3, these images could be resize */}
+          <input type="file" name="images" onChange={this.handleImagesUpload} />
+
+          {/* synopsis: few words about this item, like keys, furniture */}
+          <label htmlFor="keywords">Keywords</label>
+          <input type="text" name="keywords" id="keywords" />
+
+          {/* description: more words about this item */}
+          <label htmlFor="description">Description</label>
+          <input type="text" name="description" id="description" />
+
+          {/* location: share location or type in with zip code */}
+          <label htmlFor="location">Location</label>
+          <input type="text" name="location" id="location" />
+
+          {/* button: submit to database */}
+          <input type="submit" value="Upload Info"></input>
+        </form>
+        {/* <img src={this.state.images[0].name} alt="photos of kitten" /> */}
       </div>
+      
     );
   }
 }
